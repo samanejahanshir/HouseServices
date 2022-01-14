28048aead496077e6f6018c99634be0287a16247
samanejahanshir <jahanshir.sm92@gmail.com>
2022-01-14 12:51:02 +0330

    add mapper classes

---
+++ src/main/java/ir/maktab/dto/mapper/OfferMapper.java
@@ -0,0 +1,27 @@
+package ir.maktab.dto.mapper;
+
+import ir.maktab.data.model.Offer;
+import ir.maktab.dto.OfferDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OfferMapper {
+    public OfferDto toDto(Offer offer) {
+        return OfferDto.builder()
+                .id(offer.getId())
+                .offerCreateDate(offer.getOfferCreateDate())
+                .offerPrice(offer.getOfferPrice())
+                .durationTime(offer.getDurationTime())
+                .startTime(offer.getStartTime())
+                .build();
+    }
+    public Offer toEntity(OfferDto offerDto){
+        return  Offer.builder()
+                .offerPrice(offerDto.getOfferPrice())
+                .durationTime(offerDto.getDurationTime())
+                .id(offerDto.getId())
+                .offerCreateDate(offerDto.getOfferCreateDate())
+                .startTime(offerDto.getStartTime())
+                .build();
+    }
+}
